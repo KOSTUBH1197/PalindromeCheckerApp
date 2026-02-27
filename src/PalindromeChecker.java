c811796a087a32a82c09b455a2e3684f8cc077a7
KOSTUBH1197 <KOSTUBH1197>
2026-02-27 15:42:00 +0530

    UC3: Palindrome Check Using String Reverse

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -16,5 +16,49 @@ public class PalindromeChecker {
 
         // After printing the welcome message the program can proceed to additional use
         // cases or simply exit. For now we just demonstrate the startup behavior.
+        // UC2: Print a Hardcoded Palindrome Result
+        String testString = "madam"; // hardcoded example
+        System.out.println("Checking hardcoded string: " + testString);
+        if (isPalindrome(testString)) {
+            System.out.println(testString + " is a palindrome.");
+        } else {
+            System.out.println(testString + " is not a palindrome.");
+        }
+
+        // UC3: Palindrome Check Using String Reverse
+        String reversed = reverseString(testString);
+        System.out.println("Reversed string: " + reversed);
+        if (testString.equals(reversed)) {
+            System.out.println("(reverse-check) " + testString + " is a palindrome.");
+        } else {
+            System.out.println("(reverse-check) " + testString + " is not a palindrome.");
+        }
+    }
+
+    /**
+     * Simple helper to determine if a string is a palindrome.
+     */
+    private static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    /**
+     * Reverse the input string using a for loop.
+     */
+    private static String reverseString(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+        return reversed;
     }
 }
